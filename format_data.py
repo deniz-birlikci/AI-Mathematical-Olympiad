2b1d36fa6df421b367564cd883ef270a2cf6be22
Simrit Dhanjal <sdhanjal@simrits-mbp-2.wifi.local.cmu.edu>
2024-04-16 11:58:30 -0400

    clean and format amc and aime data

--- format_data.py
+++ format_data.py
@@ -1,10 +1,10 @@
 import json
 from util import clean_latex, get_clean_path
 """
-Cleans and formats merged_math json data for SFTTrainer
+Cleans and formats merged_math and AMC/AIME json data for SFTTrainer
 """
 
-def format_math_json(data_path):
+def format_math_problems(data_path, is_amc=False):
     """
     format single file into {"prompt": ..., "completion":...}
     data_path: single data path of where data is stored
@@ -16,14 +16,22 @@ def format_math_json(data_path):
     with open(data_path) as f:
         math_data_json_list = json.load(f)
         for math_data_json in math_data_json_list:
-            all_problems_formatted.append({
+            if is_amc:
+                # different json format
+                all_problems_formatted.append({
                 "prompt": clean_latex(math_data_json["problem"]), 
-                "completion": clean_latex(math_data_json["solution"])
-            })
+                "completion": clean_latex(math_data_json["solutions"][0]["solution"])
+                })
+            else:
+                all_problems_formatted.append({
+                    "prompt": clean_latex(math_data_json["problem"]), 
+                    "completion": clean_latex(math_data_json["solution"])
+                })
 
     return all_problems_formatted
 
-def create_formatted_math_json(data_path):
+
+def create_formatted_json(data_path, is_amc=False):
     """
     go through data path, cleans data, stores as new json file
     args:
@@ -32,7 +40,7 @@ def create_formatted_math_json(data_path):
     new_path = get_clean_path(data_path)
     # Open a file in write mode
     with open(new_path, 'w') as f:
-        json.dump(format_math_json(data_path), f, indent=4)
+        json.dump(format_math_problems(data_path, is_amc), f, indent=4)
 
 
 def get_data_SFTTrainer(data_path):
